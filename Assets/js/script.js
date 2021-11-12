009ad71a8487ac6f53c3c91e8f2961bb54d422a0
AC <alexandracampau@gmail.com>
2021-11-11 20:43:08 -0700

    adding updated index and js pseudo code

---
+++ Assets/js/script.js
@@ -0,0 +1,45 @@
+// Assignment Requirements
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+//DEFINE VARIABLES
+
+// Define a set of questions
+
+//Functions
+
+// WHEN I click the start button, timer starts
+
+// then presented with questions and choices
+
+// after question is answered, show if correct or wrong
+
+// correct answer, add 1 score to final score
+
+// wrong answer, deduct 10 second from timer
+
+// repeat with the rest of questions
+
+// if no more question, run game over function
+
+// when all questions are answered or timer reaches 0, game over
+
+// show final score
+
+// enter initials and store highscore in local storage
+
+// show current highscores
+
+// function to show high scores
+
+//Add Event Listeners
